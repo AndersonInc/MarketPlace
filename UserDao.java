6be7474e67a462d05a1d113b1bd8de2b0a67ec00
mutebitonny <98249228+mutebitonny@users.noreply.github.com>
2022-08-03 20:02:33 +0300

    Add files via upload

---
+++ UserDao.java
@@ -0,0 +1,41 @@
+package market1.dao;
+
+import java.sql.*;
+import market1.model.*;
+import market1.model.User;
+
+//this is a database object for users
+
+public class UserDao {
+	private Connection con;
+
+	private String query;
+    private PreparedStatement pst;
+    private ResultSet rs;
+
+	public UserDao(Connection con) {
+		this.con = con;
+	}
+	
+	public User userLogin(String email, String password) {
+		User user = null;
+        try {
+            query = "select * from users where email=? and password=?";
+            //this upper query help us to validate our user
+            //since we don't know these values we set them using the pst element
+            pst = this.con.prepareStatement(query);
+            pst.setString(1, email);
+            pst.setString(2, password);
+            rs = pst.executeQuery();
+            if(rs.next()){
+            	user = new User();
+            	user.setId(rs.getInt("id"));
+            	user.setName(rs.getString("name"));
+            	user.setEmail(rs.getString("email"));
+            }
+        } catch (SQLException e) {
+            System.out.print(e.getMessage());
+        }
+        return user;
+    }
+}
